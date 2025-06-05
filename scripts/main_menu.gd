e65d9d319dbc2604ea2c8e0916e33fe3d134826c
SofianeXXI <114223930+Sofivne@users.noreply.github.com>
2025-06-05 12:39:36 +0200

    creating main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,17 +1,43 @@
 extends Control
 
 @onready var Audio = $AudioStreamPlayer2D
-@onready var ButtonPlay = $"[Btn]Start"
+@onready var ButtonNewGame = $"VBoxContainer/New Game"
+@onready var ButtonLoadGame = $"VBoxContainer/Load Game"
+@onready var ButtonSettings = $"VBoxContainer/Settings"
+@onready var ButtonQuit = $"VBoxContainer/Quit"
 
 func _ready() -> void:
-	print("test")
-	if ButtonPlay:
-		print("pressed")
-		ButtonPlay.connect("pressed", Callable(self, "_on_StartButton_pressed"))
+	if ButtonNewGame:
+		ButtonNewGame.connect("pressed", Callable(self, "_on_StartButton_pressed"))
+	if ButtonLoadGame:
+		ButtonLoadGame.connect("pressed", Callable(self, "_on_LoadButton_pressed"))
+	if ButtonSettings:
+		ButtonSettings.connect("pressed", Callable(self, "_on_SettingsButton_pressed"))
+	if ButtonQuit:
+		ButtonQuit.connect("pressed", Callable(self, "_on_QuitButton_pressed"))
 	Audio.play()
 
 func _on_StartButton_pressed() -> void:
 	start_game()
 
+func _on_LoadButton_pressed() -> void:
+	load_game()
+
+func _on_SettingsButton_pressed() -> void:
+	open_settings()
+
+func _on_QuitButton_pressed() -> void:
+	quit_game()
+
 func start_game() -> void:
 	get_tree().change_scene_to_file("res://stages/stage0.tscn")
+
+func load_game() -> void:
+	print("Load game functionality here")
+
+func open_settings() -> void:
+	print("Settings menu functionality here")
+
+func quit_game() -> void:
+	print("Quit button pressed")
+	get_tree().quit()
